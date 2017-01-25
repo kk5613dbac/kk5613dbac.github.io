9ce9d78dfb0ad5bd1fef6b29aaf22e4aaed54d82
kk5613dbac <laevp88kou@yahoo.co.jp>
2017-01-25 17:02:16 +0900

    Add files via upload

--- dbac/sample10.js
+++ dbac/sample10.js
@@ -91,9 +91,9 @@ function AddAreaTypeList()
 				// 項目が空の状態で先にグループを追加すると、以降で追加した項目は強制的にグループに属するものとして扱われる
 				// → disable属性で全項目選択不可になる
 				var optGrp=document.createElement('optgroup');
-				document.getElementsByTagName("optgroup")[0].setAttribute('disabled', '');
+				document.getElementsByTagName("optgroup").setAttribute('disabled', '');
 				// style属性はgetElementsByTagName経由でアクセスしないと適用されない
-				// document.getElementsByTagName("optgroup")[0].style.display = "none";
+				// document.getElementsByTagName("optgroup").style.display = "none";
 				select1.insertBefore(optGrp, select1.firstChild);
 			}
 		});
