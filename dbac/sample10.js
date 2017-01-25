f9c57a3f39c9bd5c1bf22453e9b984da4fb0f0bf
kk5613dbac <laevp88kou@yahoo.co.jp>
2017-01-25 15:51:59 +0900

    Add files via upload

--- dbac/sample10.js
+++ dbac/sample10.js
@@ -93,9 +93,9 @@ function AddAreaTypeList()
 				var optGrp=document.createElement('optgroup');
 				select1.insertBefore(optGrp, select1.firstChild);
 				// → setAttributeの第2引数は必須
-				optGrp.getElementsByTagName("optgroup")[0].setAttribute('disabled', 'disabled');
+				select1.getElementsByTagName("optgroup")[0].setAttribute('disabled', 'disabled');
 				// style属性はgetElementsByTagName経由でアクセスしないと適用されない
-				optGrp.getElementsByTagName("optgroup")[0].style.display = "none";
+				select1.getElementsByTagName("optgroup")[0].style.display = "none";
 			}
 		});
 }
