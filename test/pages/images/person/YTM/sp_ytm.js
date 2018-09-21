b043233475fde0ecbd48f3941c20f3ed5f574a2b
kk5613dbac <laevp88kou@yahoo.co.jp>
2018-09-21 08:33:39 +0000

    Add files via upload

---
+++ test/pages/images/person/YTM/sp_ytm.js
@@ -0,0 +1,7 @@
+  (function () {
+    var tagjs = document.createElement("script");
+    var s = document.getElementsByTagName("script")[0];
+    tagjs.async = true;
+    tagjs.src = "//s.yjtag.jp/tag.js#site=13EHAMh";
+    s.parentNode.insertBefore(tagjs, s);
+  }());
