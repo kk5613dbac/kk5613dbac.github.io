0108de913859b0ed4d260fcdc29d9241bf0d0c8f
kk5613dbac <laevp88kou@yahoo.co.jp>
2019-02-25 17:26:21 +0900

    Add files via upload

---
+++ test/test190225_1.js
@@ -0,0 +1,14 @@
+﻿function ToggleTextBoxLock(obj){
+  
+  for( var i=0; i<obj.length; i++) {
+    if( obj[i].value == 'other' ) {
+      if( obj[i].selected ) {
+        document.getElementById('shop_other').disabled = false;
+      } else {
+        document.getElementById('shop_other').disabled = true;
+      }
+      break;
+    }
+  }
+  
+}
