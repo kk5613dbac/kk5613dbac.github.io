5c3e971a9abaa8c3e6a2376103c1a5c5fce4bb23
kk5613dbac <laevp88kou@yahoo.co.jp>
2019-02-26 16:02:20 +0900

    Add files via upload

---
+++ test/test190226_2.js
@@ -0,0 +1,49 @@
+﻿"use strict";	// strictモード：VBでの"Option Explicit"とほぼ同義
+
+function ToggleTextBoxLock(obj){
+  
+  for( var i=0; i<obj.length; i++) {
+    if( obj[i].value == 'その他' ) {
+      if( obj[i].selected ) {
+        document.getElementById('shop_other').disabled = false;
+        document.getElementById('shop_other').setAttribute("required", "");
+      } else {
+        document.getElementById('shop_other').disabled = true;
+        document.getElementById('shop_other').removeAttribute("required");
+      }
+      break;
+    }
+  }
+  
+}
+
+(function(){	// 即時関数（呼び出しなしで実行）
+  
+  window.addEventListener('load', function(){
+    
+    var forms = document.getElementsByClassName('needs-validation');
+　　
+    var validation = Array.prototype.filter.call(forms, function(form) {	// form要素のみ絞り込み
+      
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+      
+    });
+    
+  }, false);	// addEventListener
+  
+})();		// function
+
+function PrevPictureFile(obj){
+  
+  obj.parentNode.getElementsByClassName('custom-file-label')[0].textContent = obj.files[0].name;
+  
+  obj.parentNode.insertAdjacentHTML('afterend','<div class="preview"><div class="d-inline-block">\
+    <img class="img-thumbnail" src="' + window.URL.createObjectURL(obj.files[0]) + '"></div></div>');
+  
+}
